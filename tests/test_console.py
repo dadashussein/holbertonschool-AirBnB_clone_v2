9dc4ea38b5d3eedfaaa9aff406471a9ee6b9be48
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-03-26 13:34:30 +0400

    without db fs

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,38 +3,34 @@
 import MySQLdb
 import unittest
 import os
-from unittest import skip
+from unittest import skip, skipIf
 from console import HBNBCommand
 
 
 class TestConsole(unittest.TestCase):
     """Test console class"""
     def setUp(self):
-        self.storage_type = os.environ.get('HBNB_TYPE_STORAGE')
-        if self.storage_type == 'db':
-            self.db = MySQLdb.connect(
-                host=os.environ.get('HBNB_MYSQL_HOST'),
-                user=os.environ.get('HBNB_MYSQL_USER'),
-                passwd=os.environ.get('HBNB_MYSQL_PWD'),
-                db=os.environ.get('HBNB_MYSQL_DB')
-            )
-            create_table = """CREATE TABLE IF NOT EXISTS states (
-            id INT
-            AUTO_INCREMENT
-            PRIMARY KEY,
-            name VARCHAR(128) NOT NULL)"""
-            self.cursor = self.db.cursor()
-            self.cursor.execute("DROP TABLE IF EXISTS states")
-            self.cursor.execute(create_table)
-            self.cursor = self.db.cursor()
+        self.db = MySQLdb.connect(
+            host=os.environ.get('HBNB_MYSQL_HOST'),
+            user=os.environ.get('HBNB_MYSQL_USER'),
+            passwd=os.environ.get('HBNB_MYSQL_PWD'),
+            db=os.environ.get('HBNB_MYSQL_DB')
+        )
+        create_table = """CREATE TABLE IF NOT EXISTS states (
+        id INT
+        AUTO_INCREMENT
+        PRIMARY KEY,
+        name VARCHAR(128) NOT NULL)"""
+        self.cursor = self.db.cursor()
+        self.cursor.execute("DROP TABLE IF EXISTS states")
+        self.cursor.execute(create_table)
 
     def tearDown(self):
-        if self.storage_type == 'db':
-            self.db.close()
+        self.db.close()
 
+    @skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "Not using database")
     def test_create_state(self):
-        if self.storage_type != 'db':
-            self.skipTest("Skipping this test because storage type not 'db'")
+        """Test create State command"""
         self.cursor.execute("SELECT COUNT(*) FROM states")
         initial_count = self.cursor.fetchone()[0]
         console = HBNBCommand()
