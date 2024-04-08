191a379b555854cc4a21631f6305557d2437fd41
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-08 17:52:58 +0400

    add docstring

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,11 +10,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_session(exception):
+    """Close session"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def fetchState():
+    """Fetches states from storage"""
     states = storage.all("State").values()
     return render_template("7-states_list.html", states=states)
 
