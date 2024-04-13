6a1ce2c0df50a6367124b1640733a48729a717a8
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-13 16:34:14 +0400

    edit file extension

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,18 +8,18 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_session(exception=None):
-    """Close session"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def fetchState():
     """Fetches states from storage"""
     states = storage.all("State")
     sorted_states = sorted(states.values(), key=lambda s: s.name)
-    return render_template("7-states_list.jinja", states=sorted_states)
+    return render_template("7-states_list.html", states=sorted_states)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    """Close session"""
+    storage.close()
 
 
 if __name__ == "__main__":
