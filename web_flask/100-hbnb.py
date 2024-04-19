3cbbc21fe9c06aa4e55abfb3f8ad7db571707cc2
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-19 14:28:28 +0400

    HBNB is alive!

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """Start flask app"""
 from flask import Flask
-from models import storage
 from flask import render_template
+from models import storage
 
 
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def fetchContent():
-    """Fetching data from storage"""
+    """Display content from storage"""
     states = storage.all("State").values()
+    places = storage.all("Place").values()
     amenities = storage.all("Amenity").values()
-    sorted_state = sorted(states, key=lambda s: s.name)
-    return render_template("10-hbnb_filters.html", states=sorted_state,
-                           amenities=amenities)
+    return render_template("100-hbnb.html",  states=states,
+                           places=places, amenities=amenities)
 
 
 @app.teardown_appcontext
-def closeSession(exception):
+def closeSession(exc):
     """Close session"""
     storage.close()
 
 
-if __name__ == ("__main__"):
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
